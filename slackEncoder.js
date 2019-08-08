56d6d8a615caf4016cdb0f680ca29e10a3326f03
mderouet <martin.derouet@sicpa.com>
2019-08-08 17:05:47 +0200

    Remove hmac support, showing error message in case of decryption error

--- slackEncoder.js
+++ slackEncoder.js
@@ -1,11 +1,10 @@
 var ENCRYPTION_KEY = "DEFAULT";
 let PREFIX_MESSAGE = "@@@@";
 let HMAC_PREFIX = '####';
-
+let ERROR_MESSAGE = "[CANNOT DECODE THIS MESSAGE WITH PROVIDED ENCRYPTION KEY]";
 window.addEventListener("load", function () {
-  chrome.storage.sync.get(["USER_ENCRYPTION_KEY"], function(items) {
-    if(items.USER_ENCRYPTION_KEY)
-    {
+  chrome.storage.sync.get(["USER_ENCRYPTION_KEY"], function (items) {
+    if (items.USER_ENCRYPTION_KEY) {
       console.log('A USER ENCRYPTION KEY EXIST, SETTING IT AS CURRENT');
       ENCRYPTION_KEY = items.USER_ENCRYPTION_KEY;
     }
@@ -27,29 +26,35 @@ function decryptsAllMessages() {
   if (window.jQuery) {
     var messagesEncoded = $("span:contains(@@@@)");
     for (var i = 0; i < messagesEncoded.length; i++) {
-      let messageToDecode = messagesEncoded[i].innerText.split('@@@@')[1].split('####')[0];
-      messagesEncoded[i].innerText = decrypt(messageToDecode);
+      try {
+        let messageToDecode = messagesEncoded[i].innerText.split('@@@@')[1];
+        messagesEncoded[i].innerText = decrypt(messageToDecode);
+      } catch (error) {
+        console.log("Error decoding message " + error);
+        messagesEncoded[i].innerText = ERROR_MESSAGE 
+      }
     }
   }
 }
 
 function encrypt(message = '') {
   var encodedMessage = CryptoJS.AES.encrypt(message, ENCRYPTION_KEY);
-  encodedMessage = PREFIX_MESSAGE + encodedMessage + HMAC_PREFIX;
+  encodedMessage = PREFIX_MESSAGE + encodedMessage;
   return encodedMessage;
 }
 
 function decrypt(message = '') {
+  console.log("Trying to decrypt : " + message);
   var code = CryptoJS.AES.decrypt(message, ENCRYPTION_KEY);
   var decryptedMessage = code.toString(CryptoJS.enc.Utf8);
   return decryptedMessage;
 }
 
 chrome.runtime.onMessage.addListener(newEncryptionKey => {
-  chrome.storage.sync.set({"USER_ENCRYPTION_KEY": newEncryptionKey}, function() {
+  chrome.storage.sync.set({ "USER_ENCRYPTION_KEY": newEncryptionKey }, function () {
     alert("New encryption key set");
     console.log("USER SET A NEW ENCRYPTION KEY");
   });
 
-    ENCRYPTION_KEY = newEncryptionKey;
+  ENCRYPTION_KEY = newEncryptionKey;
 });
